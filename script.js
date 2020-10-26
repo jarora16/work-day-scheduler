57c2e00c9b251904af3985ea8643581af4aa5bb8
Jay Arora <arorajay640@gmail.com>
2020-10-26 16:26:31 -0400

    fixed the color coordinating for local storage

--- script.js
+++ script.js
@@ -1,98 +1,78 @@
-let workDay = {
-    "8 AM": "",
-    "9 AM": "",
-    "10 AM": "",
-    "11 AM": "",
-    "12 PM": "",
-    "1 PM": "",
-    "2 PM": "",
-    "3 PM": "",
-    "4 PM": "",
-    "5 PM": "",
-};
-  
- //display current day & time.
- $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-  
- 
-  $(document).ready(function(){
-    if(!localStorage.getItem('workDay')) {
-      updateCalendarTasks(workDay);
-    } else {
-      updateCalendarTasks(JSON.parse(localStorage.getItem('workDay')));
+var events;
+
+// Fill in events from local storage
+function renderEvents() {
+  // check if events have been already set
+  if (localStorage.getItem("events") !== null) {
+    // iterate over event descriptions
+    // obtain events
+    events = JSON.parse(localStorage.getItem("events"));
+    // iterate over textareas
+    $("textarea").each(function (index, element) {
+      // assign events with their descriptions
+      $(this).val(events[index + 1]);
+    });
+  } else {
+    // if events haven't been set yet, set them to empty strings
+    events = {};
+    // iterate over keys
+    for (let i = 1; i <= 9; i++) {
+      // add keys
+      events[i] = "";
     }
-  })
-  
-  $('#date-today h6').text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, h:mm:ss a'));
-  
-  let counter = 1;
-  for(const property in workDay) {
-    let textEntry = "#text-entry" + counter;
-    $(textEntry).text(workDay[property]);
-    let timeId = "#time" + counter;
-    let presentHour = moment().hour();
-    let timeString = $(timeId).text();
-    let timeNumber = hourNumberToString(timeString);  
-    if(timeNumber < presentHour) {
-      $(textEntry).addClass("past-hour");
-    } else if (timeNumber > presentHour) {
-      $(textEntry).addClass("future-hour");
-    } else {
-      $(textEntry).addClass("present-hour");
-    }
-    counter ++;
+    // log into localStorage
+    localStorage.setItem("events", JSON.stringify(events));
   }
-  
-  $("button").click(function() {
-    value = $(this).siblings("textarea").val();
-    hourString = $(this).siblings("div").text();
-    
-    saveSchedule(hourString, value);
-  });
-  
-  function hourNumberToString (hourString) {
-    switch(hourString) {
-      case "8 AM": return 8;
-      case "9 AM": return 9;
-      case "10 AM": return 10;
-      case "11 AM": return 11;
-      case "12 PM": return 12;
-      case "1 PM": return 13;
-      case "2 PM": return 14;
-      case "3 PM": return 15;
-      case "4 PM": return 16;
-      case "5 PM": return 17;
-    }
-  }
-  
-  function loadCorrectDataset() {
-    result = localStorage.getItem('workDay')
-    return (result ? result : workDay);
-  }
-  
-  function initializeLocalStorage() {
-    localStorage.setItem('workDay', JSON.stringify(workDay));
-  };
-  
-  function saveToLocalStorage(dayObj) {
-    localStorage.setItem('workDay', JSON.stringify(dayObj));
-  }
-  
-  function saveSchedule(hourString, val) {
-    if(!localStorage.getItem('workDay')) {
-      initializeLocalStorage();
-    }
-  
-    let workHours = JSON.parse(localStorage.getItem('workDay'));
-    workHours[hourString] = val
-  
-    saveToLocalStorage(workHours);
-  }
-  
-  function updateCalendarTasks(dayObject) {
-    $(".calendar-row").each(function(index) {
-      let res = $(this).children("div");
-      $(this).children("textarea").text(dayObject[res.text()]);
-    })
 }
+
+// color hour blocks
+function renderBlockColors() {
+  // obtain current hour of day
+  var hour = moment().hour();
+  
+  $("textarea").each(function (index, element) {
+    
+    var time = parseInt($(this).attr("id"));
+
+    if (time < hour) {
+      
+      $(this).addClass("past");
+    } else if (time === hour) {
+     
+      $(this).addClass("present");
+    } else {
+      
+      $(this).addClass("future");
+    }
+  });
+}
+
+// when save button is clicked
+$(".saveBtn").click(function (event) {
+  event.preventDefault();
+  // obtain event to save
+  var event = $(this).prev().val();
+  // obtain index
+  var index = $(this).attr("id");
+  // change event in global events variable
+  events[index] = event;
+  // update events in localStorage
+  localStorage.setItem("events", JSON.stringify(events));
+});
+
+function displayDay() {
+  // obtain date
+  var date = Date().substring(0, 15);
+  // display
+  $("#currentDay").text(date);
+}
+
+$(document).ready(function () {
+  displayDay();
+  renderEvents();
+  renderBlockColors();
+});
+
+//display current day & time.
+$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
